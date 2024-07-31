59b0a6efcfd0bf2cc58e8823a9059e5b61a25a65
Anubhav Jaiswal <119493362+Anubhavjaiswal21@users.noreply.github.com>
2024-07-31 11:31:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (7.97%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,34 +1,18 @@
 class Solution {
-private:
-    int solveTabulation(int m, int n) {
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-
-        dp[0][0] = 1;
-
-        for (int i = 0; i <= m; i++) {
-            for (int j = 0; j <= n; j++) {
-                if (i == 0 && j == 0) {
-                    dp[i][j] = 1;
-                    continue;
-                }
-                
-                int first = 0, second = 0;
-
-                if (i > 0)
-                    first = dp[i - 1][j];
-
-                if (j > 0)
-                    second = dp[i][j - 1];
-
-                dp[i][j] = first + second;
-            }
-        }
-        return dp[m][n];
-    }
-
 public:
     int uniquePaths(int m, int n) {
-        int ans = solveTabulation(m - 1, n - 1);
-        return ans;
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 };
