09cd85659246c10a4218851a510b8e1924b9deb7
Anubhav Jaiswal <119493362+Anubhavjaiswal21@users.noreply.github.com>
2024-09-11 12:22:20 +0530

    Time: 7 ms (70.29%) | Memory: 15.5 MB (59.34%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -14,17 +14,19 @@ public:
         if(head==NULL || head->next==NULL) return head;
         ListNode* odd=head;
         ListNode* even=head->next;
-        ListNode* evenHead=head->next;
+        ListNode* evenhead=head->next;
+        
         while(even!=NULL && even->next!=NULL)
         {
             odd->next=odd->next->next;
             even->next=even->next->next;
-            
+
             odd=odd->next;
             even=even->next;
         }
-        odd->next=evenHead;
-        return head;
-        
-    }
+        odd->next=evenhead;
+
+    
+    return head;
+}
 };
